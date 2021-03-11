7e25441916cb4cd619a1cbc4f4963e6c93db1e61
wbenica <wbenica@calpoly.edu>
2021-03-11 15:05:28 -0800

    serves publishers and series

--- db_query/views.py
+++ db_query/views.py
@@ -5,7 +5,9 @@ from django.core.serializers import serialize
 from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
-from db_query.models import GcdSeries, GcdIssue
+from db_query.models import GcdSeries, GcdIssue, GcdPublisher
+
+UNITED_STATES = 225
 
 
 def index(request):
@@ -14,15 +16,29 @@ def index(request):
 
 def series(request):
     series = GcdSeries.objects.filter(country_id=225,
-        name__contains='Doom Patrol').order_by(
-        'sort_name')
+                                      publisher__year_began__gte=1900,
+                                      publisher__series_count__gte=50).order_by('sort_name')[
+             1000:1050]
+
     series_list = serializers.serialize('json', series,
-        use_natural_foreign_keys=True)
+                                        use_natural_foreign_keys=True)
+
     return JsonResponse(json.loads(series_list), safe=False,
-        json_dumps_params={'ensure_ascii': False})
+                        json_dumps_params={'ensure_ascii': False})
 
 
 def issue(request, pk_id):
     issue = GcdIssue.objects.filter(pk=pk_id)
+
     return JsonResponse(json.loads(serialize('json', issue)), safe=False,
-        json_dumps_params={'ensure_ascii': False})
+                        json_dumps_params={'ensure_ascii': False})
+
+
+def publishers(request):
+    pub_list = GcdPublisher.objects.filter(country=UNITED_STATES,
+                                           year_began__gte=1900,
+                                           series_count__gte=50)
+
+    return JsonResponse(json.loads(serialize('json', pub_list)),
+                        safe=False,
+                        json_dumps_params={'ensure_ascii': False})
