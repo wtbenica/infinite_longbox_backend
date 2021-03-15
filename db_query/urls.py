349d761cf4771b973e2db696b86fe202fdf92997
wbenica <wbenica@calpoly.edu>
2021-03-15 12:11:36 -0700

    added filter to series request (publisher_country_id=U.S.) series list is now paginated (1000/page)

--- db_query/urls.py
+++ db_query/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('series', views.series, name='series'),
+    # path('series', views.series, name='series'),
+    path('series/<int:page>', views.series, name='seriesList'),
     path('issue/<int:pk_id>', views.issue, name='issue'),
     path('role', views.role, name='role'),
-    path('publisher', views.publishers, name='publisher')
+    path('publisher', views.publishers, name='publisher'),
+    path('series/<int:pk_id>/issues', views.issues, name='issues')
 ]
--- db_query/views.py
+++ db_query/views.py
@@ -7,6 +7,8 @@ from django.http import HttpResponse, JsonResponse
 # Create your views here.
 from db_query.models import GcdSeries, GcdIssue, GcdCreditType, GcdPublisher
 
+ROWS_PER_PAGE = 1000
+
 UNITED_STATES = 225
 
 
@@ -14,11 +16,14 @@ def index(request):
     return HttpResponse("Hello, world. You're at the polls index")
 
 
-def series(request):
-    series = GcdSeries.objects.filter(country_id=225,
+def series(request, page: int):
+    series = GcdSeries.objects.filter(country_id=UNITED_STATES,
+                                      dimensions__iregex='standard modern|standard '
+                                                                 'silver|standard gold',
+                                      publisher__country_id=UNITED_STATES,
                                       publisher__year_began__gte=1900,
-                                      publisher__series_count__gte=50).order_by('sort_name')[
-             1000:1050]
+                                      publisher__series_count__gte=200).order_by('sort_name')[
+             page * ROWS_PER_PAGE:(page + 1) * ROWS_PER_PAGE]
 
     series_list = serializers.serialize('json', series,
                                         use_natural_foreign_keys=True)
@@ -34,16 +39,24 @@ def issue(request, pk_id):
                         json_dumps_params={'ensure_ascii': False})
 
 
+def issues(request, pk_id):
+    issues = GcdIssue.objects.filter(series_id=pk_id)
+
+    return JsonResponse(json.loads(serialize('json', issues)), safe=False,
+                        json_dumps_params={'ensure_ascii': False})
+
+
 def publishers(request):
     pub_list = GcdPublisher.objects.filter(country=UNITED_STATES,
                                            year_began__gte=1900,
-                                           series_count__gte=50)
+                                           series_count__gte=200)
 
     return JsonResponse(json.loads(serialize('json', pub_list)),
                         safe=False,
                         json_dumps_params={'ensure_ascii': False})
 
+
 def role(request):
     roles = GcdCreditType.objects.all()
     return JsonResponse(json.loads(serialize('json', roles)), safe=False,
-        json_dumps_params={'ensure_ascii': False})
+                        json_dumps_params={'ensure_ascii': False})
