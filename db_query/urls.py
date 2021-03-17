0e12d166f788fd7a994c02aa3e9b02377b1a7657
wbenica <wbenica@calpoly.edu>
2021-03-16 18:44:59 -0700

    required endpoints complete

--- db_query/urls.py
+++ db_query/urls.py
@@ -3,12 +3,15 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('series', views.series, name='series'),
     path('series_list/<int:page>', views.series_list, name='series_list'),
     path('series/<int:series_id>', views.series, name='series'),
     path('series/<int:series_id>/issues', views.issues_by_series, name='issues'),
     path('issue/<int:issue_id>', views.issue, name='issue'),
-    path('issue/<int:issue_id>/credits', views.stories, name='credits'),
+    path('issue/<int:issue_id>/stories', views.stories, name='stories'),
+    path('issue/<int:issue_id>/credits', views.credits, name='credits'),
+    path('issue/<int:issue_id>/creators', views.creators, name='creators'),
     path('role', views.roles_list, name='role'),
     path('publisher', views.publishers_list, name='publisher'),
+    path('creator/<int:creator_id>', views.creator, name='creator'),
+    path('story_types', views.story_types, name='story_types'),
 ]
--- db_query/views.py
+++ db_query/views.py
@@ -1,8 +1,7 @@
 import json
 
-from django.core import serializers
 from django.core.serializers import serialize
-from django.db.models import QuerySet
+from django.db.models.query import RawQuerySet
 from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
@@ -19,19 +18,16 @@ def index(request):
 
 
 def series_list(request, page: int):
-    series = GcdSeries.objects.filter(country_id=UNITED_STATES,
-                                      dimensions__iregex='standard modern|standard '
-                                                         'silver|standard gold',
-                                      publisher__country_id=UNITED_STATES,
-                                      publisher__year_began__gte=1900,
-                                      publisher__series_count__gte=200).order_by('sort_name')[
-             page * ROWS_PER_PAGE:(page + 1) * ROWS_PER_PAGE]
+    series_list = GcdSeries.objects.filter(country_id=UNITED_STATES,
+                                           dimensions__iregex='standard modern|standard '
+                                                              'silver|standard gold',
+                                           publisher__country_id=UNITED_STATES,
+                                           publisher__year_began__gte=1900,
+                                           publisher__series_count__gte=200).order_by('sort_name')[
+                  page * ROWS_PER_PAGE:(page + 1) * ROWS_PER_PAGE]
 
-    series_list = serializers.serialize('json', series,
-                                        use_natural_foreign_keys=True)
-
-    return JsonResponse(json.loads(series_list), safe=False,
-                        json_dumps_params={'ensure_ascii': False})
+    return JsonResponse(json.loads(serialize('json', series_list, use_natural_foreign_keys=True)),
+                        safe=False, json_dumps_params={'ensure_ascii': False})
 
 
 def series(request, series_id: int):
@@ -40,39 +36,75 @@ def series(request, series_id: int):
 
 
 def issue(request, issue_id: int):
-    issue = GcdIssue.objects.filter(pk=issue_id)
-
-    return JsonResponse(json.loads(serialize('json', issue)), safe=False,
-                        json_dumps_params={'ensure_ascii': False})
+    return JsonResponse(json.loads(serialize('json', GcdIssue.objects.filter(pk=issue_id))),
+                        safe=False, json_dumps_params={'ensure_ascii': False})
 
 
 def issues_by_series(request, series_id):
-    issues = GcdIssue.objects.filter(series_id=series_id)
-
-    return JsonResponse(json.loads(serialize('json', issues)), safe=False,
-                        json_dumps_params={'ensure_ascii': False})
+    return JsonResponse(json.loads(serialize('json', GcdIssue.objects.filter(series_id=series_id))),
+                        safe=False, json_dumps_params={'ensure_ascii': False})
 
 
 def publishers_list(request) -> JsonResponse:
-    pub_list = GcdPublisher.objects.filter(country=UNITED_STATES,
-                                           year_began__gte=1900,
-                                           series_count__gte=200)
-
-    return JsonResponse(json.loads(serialize('json', pub_list)),
-                        safe=False,
-                        json_dumps_params={'ensure_ascii': False})
+    return JsonResponse(
+            json.loads(serialize('json',
+                                 GcdPublisher.objects.filter(country=UNITED_STATES,
+                                                             year_began__gte=1900,
+                                                             series_count__gte=200))),
+            safe=False,
+            json_dumps_params={'ensure_ascii': False})
 
 
 def roles_list(request):
-    roles = GcdCreditType.objects.all()
-    return JsonResponse(json.loads(serialize('json', roles)), safe=False,
+    return JsonResponse(json.loads(serialize('json', GcdCreditType.objects.all())), safe=False,
                         json_dumps_params={'ensure_ascii': False})
 
 
 def stories(request, issue_id: int):
-    stories: QuerySet[GcdStory] = GcdStory.objects.filter(issue_id=issue_id)
-    story_credits: QuerySet[GcdStoryCredit] = GcdStoryCredit.objects.filter(story__issue=issue_id)
-    return JsonResponse(json.loads(
-            "[" + serialize('json', stories) + ", " + serialize('json', story_credits) + "]"),
+    story_list = GcdStory.objects.filter(issue_id=issue_id)
+
+    return JsonResponse(
+            json.loads(serialize('json', story_list)),
+            safe=False,
+            json_dumps_params={'ensure_ascii': False})
+
+
+def credits(request, issue_id: int):
+    story_credits = GcdStoryCredit.objects.filter(story__issue=issue_id)
+
+    js = JsonResponse(
+            json.loads(serialize('json', story_credits)),
+            safe=False,
+            json_dumps_params={'ensure_ascii': False})
+
+    return js
+
+
+def creator(request, creator_id: int):
+    return JsonResponse(json.loads(serialize('json', GcdCreator.objects.filter(pk=creator_id))),
+                        safe=False,
+                        json_dumps_params={'ensure_ascii': False})
+
+
+def story_types(request):
+    return JsonResponse(json.loads(serialize('json', GcdStoryType.objects.all())),
+                        safe=False,
+                        json_dumps_params={'ensure_ascii': False})
+
+
+def creators(request, issue_id):
+    story_list: RawQuerySet = GcdCreator.objects.raw(f"""
+        select *
+        from gcd_creator gc
+        where gc.id in (
+            select gsc.creator_id
+            from gcd_story_credit gsc
+            where gsc.story_id in (
+                select gs.id
+                from gcd_story gs
+                where gs.issue_id = {issue_id}))""")
+
+    return JsonResponse(
+            json.loads(serialize('json', story_list)),
             safe=False,
             json_dumps_params={'ensure_ascii': False})
